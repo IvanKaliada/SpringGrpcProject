9e429e1684c430e5b53ae662d7740db16483f52c
Ivan Kaliada Esure <i.kaliada@eis-partners.com>
2023-10-25 12:16:29 +0200

    Add client side

--- src/main/java/com/iikaliada/spring/grpc/project/Application.java
+++ src/main/java/com/iikaliada/spring/grpc/project/Application.java
@@ -1,13 +1,26 @@
 package com.iikaliada.spring.grpc.project;
 
+import com.iikaliada.spring.grpc.project.client.EmployeeClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private EmployeeClientService employeeClientService;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        int id = 1;
+        String response = employeeClientService.getEmployeeRequest(id);
+        System.out.println("Response: " + response);
+    }
 }
